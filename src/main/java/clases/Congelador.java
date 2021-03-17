4c4fcaddc6ec24e99b0e8b231c0e5e9b0c730f26
andre <andre@MSI>
2021-03-17 20:01:40 +0100

    hecho usuario+congelador enum

--- src/main/java/clases/Congelador.java
+++ src/main/java/clases/Congelador.java
@@ -19,12 +19,43 @@ public class Congelador extends Electrodomestico{
     public Congelador(){}
 
     public Congelador(String Marca, int AlmacenamientoAveria, String Comentarios, 
-            ClaseE calificacionEnergetica, String gama, String codigo, String nombre,
-            float precio, int stock) {
+        ClaseE calificacionEnergetica, String gama, String codigo, String nombre,
+        float precio, int stock) {
         super(calificacionEnergetica, gama, codigo, nombre, precio, stock);
         this.Marca = Marca;
         this.AlmacenamientoAveria = AlmacenamientoAveria;
         this.Comentarios = Comentarios;
+    }
+    @Override
+    public int compareTo(Congelador c){
+        if (this.getCalificacionEnergetica().equals(c.getCalificacionEnergetica())) {
+            return 0;
+        }
+        int aux1=0;
+        int aux2=0;
+        switch(this.getCalificacionEnergetica()){
+            case A:aux1=1;break;
+            case B:aux1=2;break;
+            case C:aux1=3;break;
+            case D:aux1=4;break;
+            case E:aux1=5;break;
+            case F:aux1=5;break;
+            
+        }
+        switch(c.getCalificacionEnergetica()){
+            case A:aux1=1;break;
+            case B:aux1=2;break;
+            case C:aux1=3;break;
+            case D:aux1=4;break;
+            case E:aux1=5;break;
+            case F:aux1=5;break;
+            
+        }
+        if (aux1>aux2) {
+            return 1;
+        }else{
+            return -1;
+        }
     }
     @Override
     public String toString(){
